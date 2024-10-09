a1cdcbf8fb46b21ee3aa47579fc8655b3c17c30e
CodeName33 <52039381+CodeName33@users.noreply.github.com>
2024-10-09 19:36:39 +0300

    Add files via upload

--- Sources/ESP32S3/USB-Mouse-Keyboard.ino
+++ Sources/ESP32S3/USB-Mouse-Keyboard.ino
@@ -1,5 +1,27 @@
-#define ARDUINO_USB_MODE 2
+/*
+  Keyboard test
 
+  Reads a byte from the serial port, sends a keystroke back.
+  The sent keystroke is one higher than what's received, e.g. if you send a,
+  you get b, send A you get B, and so forth.
+
+  The circuit:
+  - none
+
+  created 21 Oct 2011
+  modified 27 Mar 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/KeyboardSerial
+*/
+
+
+//#define ARDUINO_USB_MODE 2
+//#define SOC_USB_OTG_SUPPORTED
+
+/*
 #ifndef ARDUINO_USB_MODE
 #error This ESP32 SoC has no Native USB interface
 #elif ARDUINO_USB_MODE == 1
@@ -7,12 +29,14 @@
 void setup() {}
 void loop() {}
 #else
+*/
 
 #include "USB.h"
 #include "USBHIDKeyboard.h"
 #include "USBHIDMouse.h"
 USBHIDKeyboard Keyboard;
 USBHIDAbsoluteMouse Mouse;
+//USBHIDRelativeMouse Mouse;
 int mx = 0;
 int my = 0;
 #define RESET_BUTTON_PIN 17
@@ -27,17 +51,40 @@ void setup() {
     pinMode(POWER_BUTTON_PIN, OUTPUT);
     digitalWrite(POWER_BUTTON_PIN, 0);
   }
+/*
+  pinMode(17, OUTPUT);
+  pinMode(18, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(3, OUTPUT);
+  digitalWrite(17, 1);
+  digitalWrite(18, 1);
+  digitalWrite(8, 1);
+  digitalWrite(3, 1);
 
+  pinMode(35, OUTPUT);
+  pinMode(36, OUTPUT);
+  pinMode(37, OUTPUT);
+  pinMode(38, OUTPUT);
+  digitalWrite(35, 1);
+  digitalWrite(36, 1);
+  digitalWrite(37, 1);
+  digitalWrite(38, 1);
+  */
 
   Serial.begin(115200);
   Keyboard.begin();
   Mouse.begin();
   USB.begin();
+
+  delay(1000);
+  Serial.write("Started!");
 }
 
 void loop() {
     if (Serial.available() > 0) {
         String line = Serial.readStringUntil('\n');
+        Serial.write(line.c_str());
+        Serial.write("\n");
         if (line.endsWith("\r")) {
             line = line.substring(0, line.length() - 1);
         }
@@ -106,16 +153,16 @@ void loop() {
         } else if (line.startsWith("RB:")) {
           if (RESET_BUTTON_PIN) {
             digitalWrite(RESET_BUTTON_PIN, 1);
-            delay(100);
+            delay(250);
             digitalWrite(RESET_BUTTON_PIN, 0);
           }
         } else if (line.startsWith("PW:")) {
           if (POWER_BUTTON_PIN) {
             digitalWrite(POWER_BUTTON_PIN, 1);
-            delay(100);
+            delay(250);
             digitalWrite(POWER_BUTTON_PIN, 0);
           }
         }
     }
 }
-#endif
+//#endif /* ARDUINO_USB_MODE */
